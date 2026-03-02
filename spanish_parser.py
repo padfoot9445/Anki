5dbe3d32f72b0672707f74928f4cd8b8487ca3ca
padfoot9445 <yipy214@gmail.com>
2026-03-02 20:13:21 +0000

    handled multiple brackets

--- spanish_parser.py
+++ spanish_parser.py
@@ -1,15 +1,48 @@
 def line_to_word_pair(line: str) -> tuple[str, str]:
     if line[-1] == "*":
         line = line[:-1]
-    current = 0
-    try:
-        while line[current] != '(':
-            current += 1
-    except IndexError:
-        print(line)
-        raise
+    
+    start = current = 0
+    
+    while True:
+        x = find_bracket(line, current)
+        if x == None:
+            break
+        else:
+            start, current = x
 
-    return line[:current].strip(), line[current + 1: -1].strip()
+    return line[:start].strip(), line[start + 1: -1].strip()
+
+def find_bracket(line: str, start: int) -> tuple[int, int] | None:
+    start = line.find('(', start)
+
+    if start == -1:
+        return None
+
+    try:
+        stack = 0
+        current = start
+
+        assert line[current] == '(', f"expected '('; got {line[current]} at {current} in {line}"
+
+        while True:
+            if line[current] == '(':
+                stack += 1
+            elif line[current] == ')':
+                stack -= 1
+            if stack == 0:
+                break
+            current += 1
+        
+        return start, current
+    except IndexError:
+        print(f"Unpaired opening bracket in section beginning {start} in {line}")
+        return None
+
+
+
+
+
 
 
 
